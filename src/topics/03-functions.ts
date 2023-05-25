50830c640d4a9a8f4624e892937df558703f1070
Campuzano90 <91767773+Campuzano90@users.noreply.github.com>
2023-05-25 11:56:45 -0600

    1 file modified

--- src/topics/03-functions.ts
+++ src/topics/03-functions.ts
@@ -6,11 +6,21 @@ const addNumbersArrow = (a: number, b: number): string => {
   return `${a + b}`;
 };
 
-function multiply(firstNumber: number, secondNumber: number, base: number) {
-  return firstNumber + base;
+function multiply(
+  firstNumber: number,
+  secondNumber?: number,
+  base: number = 2
+) {
+  return firstNumber * base;
 }
 
-const result: number = addNumbers(1, 2);
-const resul2: string = addNumbersArrow(1, 2);
-console.log({ result, resul2 });
+//const result: number = addNumbers(1, 2);
+//const resul2: string = addNumbersArrow(1, 2);
+//const multiplyResult: number = multiply(5);
+//console.log({ result, resul2, multiplyResult });
+
+const healCharacter = (character, amount) => {
+  character.pv += amount;
+};
+
 export {};
