e22b0bd1b9ed06c05889ab889c9746114d0e45ca
Campuzano90 <91767773+Campuzano90@users.noreply.github.com>
2023-05-25 12:07:22 -0600

    1 file modified

--- src/topics/03-functions.ts
+++ src/topics/03-functions.ts
@@ -19,8 +19,25 @@ function multiply(
 //const multiplyResult: number = multiply(5);
 //console.log({ result, resul2, multiplyResult });
 
-const healCharacter = (character, amount) => {
-  character.pv += amount;
+interface Character {
+  name: string;
+  hp: number;
+  showHp: () => void;
+}
+
+const healCharacter = (character: Character, amount: number) => {
+  character.hp += amount;
 };
+
+const strider: Character = {
+  name: 'Strider',
+  hp: 50,
+  showHp() {
+    console.log(`Puntos de Vida ${this.hp}`);
+  },
+};
+
+healCharacter(strider, 10);
+strider.showHp();
 
 export {};
