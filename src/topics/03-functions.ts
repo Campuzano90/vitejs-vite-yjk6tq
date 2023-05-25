c0618a830e71a6d6dbe2d0ec3feaa9cdef6a7ba7
Campuzano90 <91767773+Campuzano90@users.noreply.github.com>
2023-05-25 11:45:46 -0600

    1 file modified

--- src/topics/03-functions.ts
+++ src/topics/03-functions.ts
@@ -6,6 +6,10 @@ const addNumbersArrow = (a: number, b: number): string => {
   return `${a + b}`;
 };
 
+function multiply(firstNumber: number, secondNumber: number, base: number) {
+  return firstNumber + base;
+}
+
 const result: number = addNumbers(1, 2);
 const resul2: string = addNumbersArrow(1, 2);
 console.log({ result, resul2 });
