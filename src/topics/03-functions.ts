6bc64e86255465b7b40f3b54fe26747c9d542d52
Campuzano90 <91767773+Campuzano90@users.noreply.github.com>
2023-05-25 11:43:28 -0600

    2 files modified

--- src/topics/03-functions.ts
+++ src/topics/03-functions.ts
@@ -1,7 +1,12 @@
-function addNumbers(a: number, b: number) {
+function addNumbers(a: number, b: number): number {
   return a + b;
 }
 
+const addNumbersArrow = (a: number, b: number): string => {
+  return `${a + b}`;
+};
+
 const result: number = addNumbers(1, 2);
-console.log(result);
+const resul2: string = addNumbersArrow(1, 2);
+console.log({ result, resul2 });
 export {};
