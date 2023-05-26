4d86e0156d8406fb128875630c6703d5ed67095b
Campuzano90 <91767773+Campuzano90@users.noreply.github.com>
2023-05-26 11:53:43 -0600

    3 files modified, 1 added

--- src/topics/01-basic-types.ts
+++ src/topics/01-basic-types.ts
@@ -1,4 +1,13 @@
-let name: string = 'Carlos';
-let hpPoints: number | string = 95;
+const name: string = 'Strider';
+let hpPoints: number | 'FULL' = 95;
+const isAlive: boolean = true;
+
+hpPoints = 'FULL';
+
+console.log({
+  name,
+  hpPoints,
+  isAlive,
+});
 
 export {};
