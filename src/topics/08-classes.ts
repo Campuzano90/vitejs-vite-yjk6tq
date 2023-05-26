091b9813c521d530696624eefb994d2c673abf18
Campuzano90 <91767773+Campuzano90@users.noreply.github.com>
2023-05-26 10:58:29 -0600

    2 files modified, 1 added

--- src/topics/08-classes.ts
+++ src/topics/08-classes.ts
@@ -1,9 +1,36 @@
 export class Person {
-  public name: String;
-  public address: String;
+  //public name: String;
+  //public address: String;
 
-  constructor() {
-    this.name = 'Carlos';
-    this.address = 'Garden Juarez';
+  constructor(
+    public firsName: string,
+    public lastName: string,
+    private address: string = 'No address'
+  ) {}
+}
+
+/*export class Hero extends Person {
+  constructor(
+    public alterEgo: String,
+    public age: number,
+    public realName: string
+  ) {
+    super(realName, 'New York');
+  }
+}*/
+
+export class Hero {
+  //public person: Person;
+
+  constructor(
+    public alterEgo: String,
+    public age: number,
+    public realName: string,
+    public person: Person
+  ) {
+    //this.person = new Person(realName);
   }
 }
+const tony = new Person('Tony', 'Star', 'New York');
+const herue = new Hero('Iron Man', 45, 'Tony', tony);
+console.log(herue);
---
+++ src/topics/09-generics.ts
@@ -0,0 +1,10 @@
+export function whatsMyType<T>(argument: T): T {
+  return argument;
+}
+
+const amIString = whatsMyType<string>('Hola mundo');
+const amINumber = whatsMyType<number>(100);
+const amIArray = whatsMyType<number[]>([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+console.log(amIString.split(' '));
+console.log(amINumber.toFixed());
+console.log(amIArray.join('-'));
